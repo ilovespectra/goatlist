1ce38981adb7117ce70592e947ba73c0b2e91d15
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-06-14 07:51:29 -0400

    add edit / delete functions

--- src/components/Goatlist.tsx
+++ src/components/Goatlist.tsx
@@ -1,13 +1,16 @@
 import { initializeApp, FirebaseApp, getApp } from 'firebase/app';
-import { getFirestore, collection, getDocs, deleteDoc, doc, DocumentData } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import React, { useState, useEffect } from 'react';
 import { saveAs } from 'file-saver';
 import { useWallet } from '@solana/wallet-adapter-react';
-import { FaTimes } from 'react-icons/fa'; // Install react-icons if you haven't already
-import 'tailwindcss/tailwind.css'; // Assuming you're using Tailwind CSS
+import { FaTrash, FaEdit, FaSave, FaTimes } from 'react-icons/fa';
+
+type DocumentData = any;
 
 function ViewEntries() {
     const [entryList, setEntryList] = useState<DocumentData[]>([]);
+    const [editingId, setEditingId] = useState<string | null>(null);
+    const [newImageUrl, setNewImageUrl] = useState<string>('');
     const { publicKey } = useWallet();
 
     const firebaseConfig = {
@@ -20,18 +23,18 @@ function ViewEntries() {
         measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
     };
 
-    // Initialize Firebase if not already initialized
     let app: FirebaseApp;
     try {
         app = initializeApp(firebaseConfig);
     } catch (error) {
-        app = getApp(); // If Firebase app is already initialized, get it
+        app = getApp();
     }
+
     const db = getFirestore(app);
 
     const fetchAndDisplayEntries = () => {
         const users: DocumentData[] = [];
-        const usersCollection = collection(db, 'users'); // Changed collection name to 'users'
+        const usersCollection = collection(db, 'users');
         
         getDocs(usersCollection)
             .then((querySnapshot) => {
@@ -48,48 +51,104 @@ function ViewEntries() {
 
     useEffect(() => {
         fetchAndDisplayEntries();
-    }, []); // Fetch and display entries when component mounts
+    }, []);
+
+    const handleDeleteEntry = async (id: string) => {
+        if (window.confirm('Are you sure you want to delete this entry?')) {
+            const userDoc = doc(db, 'users', id);
+            try {
+                await deleteDoc(userDoc);
+                setEntryList(entryList.filter(entry => entry.id !== id));
+            } catch (error) {
+                console.error('Error deleting document: ', error);
+            }
+        }
+    };
+
+    const handleUpdateEntry = async (id: string) => {
+        const userDoc = doc(db, 'users', id);
+        try {
+            await updateDoc(userDoc, { image: newImageUrl });
+            setEntryList(entryList.map(entry => (entry.id === id ? { ...entry, image: newImageUrl } : entry)));
+            setEditingId(null);
+            setNewImageUrl('');
+        } catch (error) {
+            console.error('Error updating document: ', error);
+        }
+    };
 
     const handleDownloadPubkeys = () => {
-        const uniquePubkeys = new Set(entryList.map(entry => entry.pubkey));
+        const uniquePubkeys = new Set<string>();
+        entryList.forEach(entry => uniquePubkeys.add(entry.pubkey));
         const pubkeys = [...uniquePubkeys].join(', ');
         const blob = new Blob([pubkeys], { type: 'text/plain;charset=utf-8' });
         saveAs(blob, 'goatlist.txt');
     };
 
-    const handleDeleteEntry = async (id: string) => {
-        await deleteDoc(doc(db, 'users', id));
-        fetchAndDisplayEntries();
-    };
-
     return (
         <div className="container">
-            <button className="border border-gray rounded-lg py-2 px-4 mt-3 mb-3 hover:scale-105" onClick={handleDownloadPubkeys}>
-                save wallet list
+            <button 
+                className="border border-gray rounded-lg py-2 px-4 mt-3 mb-3 hover:scale-105" 
+                onClick={handleDownloadPubkeys}
+            >
+                Save Wallet List
             </button>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {entryList.map((entry, index) => (
-                    <div key={index} className="relative bg-transparent border-2 border-gray-500 rounded-lg p-4 transition-transform transform hover:scale-105">
-                        {publicKey?.toBase58() === entry.pubkey && (
-                            <button 
-                                onClick={() => handleDeleteEntry(entry.id)} 
-                                className="absolute top-2 right-2 text-red-500"
-                            >
-                                <FaTimes />
-                            </button>
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))', gap: '10px' }}>
+                {entryList.map((entry) => (
+                    <div key={entry.id} className="text-center bg-transparent border-2 border-gray rounded-lg p-4 transition-transform transform hover:scale-105">
+                        {editingId === entry.id ? (
+                            <>
+                                <input 
+                                    type="text" 
+                                    value={newImageUrl} 
+                                    onChange={(e) => setNewImageUrl(e.target.value)} 
+                                    placeholder="img url" 
+                                    className="mt-2 p-2 rounded-lg border-2 border-gray-300 w-full bg-black text-white" 
+                                />
+                                <button 
+                                    onClick={() => handleUpdateEntry(entry.id)} 
+                                    className="mt-2 p-2 bg-purple-500 hover:bg-purple-800 text-white rounded-lg w-full flex justify-center items-center"
+                                >
+                                    <FaSave className="mr-2" /> save
+                                </button>
+                                <button 
+                                    onClick={() => {
+                                        setEditingId(null);
+                                        setNewImageUrl('');
+                                    }} 
+                                    className="mt-2 p-2 bg-blue-600 hover:bg-blue-800 text-white rounded-lg w-full flex justify-center items-center"
+                                >
+                                    <FaTimes className="mr-2" /> cancel
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <a
+                                    href={`https://x.com/${entry.twitterHandle}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer nofollow"
+                                    className="text-center"
+                                >
+                                    <img src={entry.image} alt="Profile" className="w-24 h-24 rounded-full mx-auto" />
+                                    <p className="text-white text-lg overflow-wrap break-word">
+                                        <strong>{entry.twitterHandle}</strong>
+                                    </p>
+                                    <p className="text-white">{entry.nickname}</p>
+                                </a>
+                                {publicKey && publicKey.toString() === entry.pubkey && (
+                                    <div className="flex justify-between mt-2">
+                                        <FaEdit 
+                                            onClick={() => setEditingId(entry.id)} 
+                                            className="cursor-pointer text-blue-600 hover:text-blue-800"
+                                        />
+                                        <FaTrash 
+                                            onClick={() => handleDeleteEntry(entry.id)} 
+                                            className="cursor-pointer text-purple-600 hover:text-purple-800"
+                                        />
+                                    </div>
+                                )}
+                            </>
                         )}
-                        <a
-                            href={`https://x.com/${entry.twitterHandle}`}
-                            target="_blank"
-                            rel="noopener noreferrer nofollow"
-                            className="text-center"
-                        >
-                            <img src={entry.image} alt="Profile" className="w-24 h-24 rounded-full mx-auto" />
-                            <p className="text-white text-lg overflow-wrap break-word">
-                                <strong>{entry.twitterHandle}</strong>
-                            </p>
-                            <p className="text-white">{entry.nickname}</p>
-                        </a>
                     </div>
                 ))}
             </div>
